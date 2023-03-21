bd3cf6cdb7f15d0e7ad302b020a37d6293c132cc
mourad denguir <denguirmourad0@gmail.com>
2023-03-21 17:20:55 +0000

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * _islower - check alpha
- *
+ * @c: the parametre
  * Return: 1 if is alpha else 0
  */
 int _isalpha(int c)
 {
-	if (c <= 'z' && c >= 'a' || c <= 'Z' && c >= 'A')
+	if ((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A'))
 		return (1);
 	return (0);
 }
