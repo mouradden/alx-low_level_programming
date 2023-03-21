f0445cc4bf16798ef6459810ce492765506d3742
mourad denguir <denguirmourad0@gmail.com>
2023-03-21 16:38:52 +0000

    fix a problem

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - display the sentence
+ *
+ * Return: 0 means exit with success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+README of 0x01-variables_if_else_while
