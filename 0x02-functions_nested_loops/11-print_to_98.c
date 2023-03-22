34c345a8fb151aae44230ddb6ed356aa300d1a65
mourad denguir <denguirmourad0@gmail.com>
2023-03-22 00:08:16 +0000

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		if (i <= 9)
+			_putchar(i + '0');
+		else
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+		}
+		if (i < 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
