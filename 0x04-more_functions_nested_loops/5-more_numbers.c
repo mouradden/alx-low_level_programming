be01e8abf7c84a3c3d1ff38a5904b0a24d4d42bd
mourad denguir <denguirmourad0@gmail.com>
2023-03-23 14:21:43 +0000

    fix task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,12 +5,22 @@
 void more_numbers(void)
 {
 	int	i;
+	int n;
 
-	for (i = 0; i <= 14; i++)
+	for (n = 0; n <= 9; n++)
 	{
-		if (i >= 10)
-			_putchar(i / 10 + '0');
-		_putchar(i % 10 + '0');
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+				putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+		}
+		putchar('\n');
 	}
-	_putchar('\n');
+	
+}
+int main(void)
+{
+    more_numbers();
+    return (0);
 }
