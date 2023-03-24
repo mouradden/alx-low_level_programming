9a3edf374d7b14c96897197f19912b3ab0f3a1b8
mourad denguir <denguirmourad0@gmail.com>
2023-03-24 23:27:33 +0000

    task 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,7 +5,13 @@
  */
 void print_number(int n)
 {
-	if (n >= 0 && n <= 9)
+	if (n = -2147483648)
+	{
+		_putchar('-');
+		_putchar(2);
+		print_number(147483648);
+	}
+	else if (n >= 0 && n <= 9)
 		_putchar(n + '0');
 	else if (n < 0)
 	{
