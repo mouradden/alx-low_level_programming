7575ba7a20b40a87e9652638daf6bbeb8c04e1fc
mourad denguir <denguirmourad0@gmail.com>
2023-03-28 15:06:51 +0000

    task 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,9 +15,9 @@ int _atoi(char *s)
 	signe = 1;
 	while (s[i])
 	{
-		while ((s[i] != '-' && s[i] != '+') && (s[i] < '0' || s[i] > '9'))
+		while (s[i] == ' ' || ((s[i] != '-' && s[i] != '+') && (s[i] < '0' || s[i] > '9')))
 			i++;
-		while (s[i] == ' ' || s[i] == '-' || s[i] == '+')
+		while (s[i] == '-' || s[i] == '+')
 		{
 			if (s[i] == '-')
 				signe *= -1;
