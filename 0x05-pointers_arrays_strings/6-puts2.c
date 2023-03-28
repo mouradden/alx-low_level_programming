a75dae155d207883fd4fdb181a1603d4fad6aa37
mourad denguir <denguirmourad0@gmail.com>
2023-03-28 01:34:11 +0000

    tasks

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,10 +13,14 @@ void puts2(char *str)
 	{
 		len++;
 	}
-	i = (len - 1) / 2;
-	while (str[i + 1])
+	len --;
+	if (len % 2 == 0)
+		i = len / 2;
+	else
+		i = (len - 1) / 2;
+	while (str[i])
 	{
-		write(1, &str[i + 1], 1);
+		write(1, &str[i], 1);
 		i++;
 	}
 	write(1, "\n", 1);
