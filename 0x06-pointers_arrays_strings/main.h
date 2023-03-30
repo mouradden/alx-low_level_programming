f3ef50f81de7ae60f5aa655ead91a0046cbdc63e
mourad denguir <denguirmourad0@gmail.com>
2023-03-30 12:57:22 +0000

    commit tasks 0 1 2 3

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+void _putchar(int c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+
+#endif
