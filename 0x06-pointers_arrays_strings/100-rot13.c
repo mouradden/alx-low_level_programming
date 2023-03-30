0caf28331964fde71f4b9f8f1c5a5b7fe7b2f8ee
mourad denguir <denguirmourad0@gmail.com>
2023-03-30 23:53:54 +0000

    task 100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -15,7 +15,7 @@ char *rot13(char *str)
 		if ((str[i] >= 'A' && str[i] <= 'M') ||
 			(str[i] >= 'a' && str[i] <= 'm'))
 			str[i] += 13;
-		else if ((str[i]c >= 'N' && str[i] <= 'Z') ||
+		else if ((str[i] >= 'N' && str[i] <= 'Z') ||
 			(str[i] >= 'n' && str[i] <= 'z'))
 			str[i] -= 13;
 		i++;
