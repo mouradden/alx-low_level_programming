3a99811d30911e4ca195d55db6b1d44688c76d02
mourad denguir <denguirmourad0@gmail.com>
2023-03-30 23:11:50 +0000

    fix task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,7 +15,11 @@ char *cap_string(char *str)
 		str[0] -= 32;
 	while (str[i])
 	{
-		if ((str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '\"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '}' || str[i - 1] == '{') && (str[i] >= 'a' && str[i] <= 'z'))
+		if ((str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+			str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+			str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '\"' ||
+			str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '}' ||
+			str[i - 1] == '{') && (str[i] >= 'a' && str[i] <= 'z'))
 			str[i] -= 32;
 		i++;
 	}
