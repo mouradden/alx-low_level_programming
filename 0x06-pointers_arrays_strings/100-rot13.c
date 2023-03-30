b504ff57354c3659c2dca8c08d77b10bdc5249d2
mourad denguir <denguirmourad0@gmail.com>
2023-03-30 23:52:12 +0000

    task 100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rot13 - shift up string with 13
+ * @str: string
+ * Return: string converted
+ */
+char *rot13(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if ((str[i] >= 'A' && str[i] <= 'M') ||
+			(str[i] >= 'a' && str[i] <= 'm'))
+			str[i] += 13;
+		else if ((str[i]c >= 'N' && str[i] <= 'Z') ||
+			(str[i] >= 'n' && str[i] <= 'z'))
+			str[i] -= 13;
+		i++;
+	}
+	return (str);
+}
