6a20fec22b47d8afe1e04f46e4e1118d8c9376eb
mourad denguir <denguirmourad0@gmail.com>
2023-04-09 22:15:21 +0000

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - program displays program name
+ * @ac: numbre of  args
+ * @av: arguments
+ * Return: 0
+ */
+
+int main(int ac, char **av)
+{
+	printf("%s", av[0]);
+	return (0);
+}
