24c5f9af5eb8eb4a834e00e159ec939ca9f0264b
mourad denguir <denguirmourad0@gmail.com>
2023-04-09 23:01:50 +0000

    task 0

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,13 +2,14 @@
 
 /**
  * main - program displays number of args except the program name
- * @ac: numbre of  args
+ * @ac: numbre of args
  * @av: arguments
  * Return: 0
  */
 
 int main(int ac, char **av)
 {
+	(void)av;
 	printf("%d\n", ac - 1);
 	return (0);
 }
--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,6 +11,7 @@ int main(int ac, char **av)
 {
 	int i;
 
+	(void)ac;
 	i = 0;
 	while (av[i])
 	{
