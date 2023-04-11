8dc6b4be30d6396d8d45c7755c8f1bdbda2f9868
mourad denguir <denguirmourad0@gmail.com>
2023-04-11 18:45:32 +0000

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,24 +12,24 @@ char *str_concat(char *s1, char *s2)
 	char *res;
 
 	i = 0;
-	while (dest[i] != '\0')
+	while (s1[i] != '\0')
 		i++;
 	j = 0;
-	while (src[j] != '\0')
+	while (S2[j] != '\0')
 		j++;
 	res = malloc(i + j + 1);
 	j = 0;
 	i = 0;
-	while (src[j] != '\0')
+	while (s1[j] != '\0')
 	{
-		res[i] = src[j];
+		res[i] = s1[j];
 		i++;
 		j++;
 	}
 	j = 0;
-	while (dst[j] != '\0')
+	while (s2[j] != '\0')
 	{
-		res[i] = dst[j];
+		res[i] = s2[j];
 		i++;
 		j++;
 	}
