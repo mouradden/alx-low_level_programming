3f0922d56b1c82344fe36d24cd5b316f872c3380
mourad denguir <denguirmourad0@gmail.com>
2023-04-13 23:57:02 +0000

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,6 +26,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	str = malloc(sizeof(char) * (len1 + len2) + 1);
 	if (!str)
 		return (NULL);
+	i = 0;
 	while (s1[i])
 	{
 		str[i] = s1[i];
