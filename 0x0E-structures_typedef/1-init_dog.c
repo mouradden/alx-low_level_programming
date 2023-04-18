5b2731bc8c30c6f9b6ec593f9a7093a20b7f33c5
mourad denguir <denguirmourad0@gmail.com>
2023-04-18 22:26:56 +0000

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * init_dog - 
+ * @d: the struct
+ * @name: the name to assign to struct
+ * @age: the age to assign to struct
+ * @owner: the owner to assign to struct
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,6 +11,8 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-}	dog_t;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
