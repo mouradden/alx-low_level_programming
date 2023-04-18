e6428c3096d0a51c81407c3231ad8548f058b367
mourad denguir <denguirmourad0@gmail.com>
2023-04-18 18:52:09 +0000

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
-
-struct dog {
+/**
+ * struct dog - dog data
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the owner of the dog
+ */
+typedef struct dog {
 	char *name;
 	float age;
 	char *owner;
