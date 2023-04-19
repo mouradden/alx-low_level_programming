8e22360e7d50e84043d38c1d6a13b140d470d505
mourad denguir <denguirmourad0@gmail.com>
2023-04-19 01:28:17 +0000

    task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+
 #include <stdlib.h>
 #include <stdio.h>
 /**
@@ -9,7 +10,6 @@
  * @owner: owner
  *
  * Description: description
- *
  */
 struct dog
 {
@@ -17,14 +17,12 @@ struct dog
 	float age;
 	char *owner;
 };
+
 /**
  * dog_t - typedef for struct dog
  */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 #endif
