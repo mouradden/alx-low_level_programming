b422289e8d5229f82608076a7d2e42533a1ea7f2
mourad denguir <denguirmourad0@gmail.com>
2023-04-19 01:14:26 +0000

    task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,14 +5,13 @@
  * @name: the name to assign to struct
  * @age: the age to assign to struct
  * @owner: the owner to assign to struct
- * Return: nothing
+ * Return: NOTHING
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+	if (!d)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
