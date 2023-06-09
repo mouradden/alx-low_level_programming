f79562e1af21637440450804285a7c03d2791d39
mourad denguir <denguirmourad0@gmail.com>
2023-06-09 21:50:08 +0100

    tasks

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _atoi - copy from src to dst
+ * @s: parametre
+ * Return: integer converted
+ */
+int _atoi(char *s)
+{
+	int i;
+	int res;
+	int signe;
+	
+	res = 0;
+	i = 0;
+	signe = 1;
+	while (s[i])
+	{
+		while (s[i] == ' ' || ((s[i] != '-' && s[i] != '+') && (s[i] < '0' || s[i] > '9')))
+			i++;
+		while (s[i] == '-' || s[i] == '+')
+		{
+			if (s[i] == '-')
+				signe *= -1;
+			i++;
+		}
+		while (s[i] >= '0' && s[i] <= '9')
+		{
+			res = res * 10 + (s[i] - '0');
+			i++;
+		}
+	}
+	
+	return res*signe;
+}
---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _putchar - fill s by b, n bytes
+ * Return: int
+ */
+int _putchar(char c)
+{
+	
+}
